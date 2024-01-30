bf594494bc03a2bc9bb8c77dc5c6bdedb985ab84
Susana Nunez Zamora <snunez-z@c2r8s3.42madrid.com>
2024-01-30 17:17:55 +0100

    Semana 4

--- cursustelelana/libft.a/ft_strlcpy.c
+++ cursustelelana/libft.a/ft_strlcpy.c
@@ -6,40 +6,42 @@
 /*   By: snunez-z <snunez-z@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:19:05 by snunez-z          #+#    #+#             */
-/*   Updated: 2024/01/12 13:46:42 by snunez-z         ###   ########.fr       */
+/*   Updated: 2024/01/30 14:31:22 by snunez-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
+/*#include <stdio.h>*/
+#include "libft.h"
 
 size_t	ft_strlcpy(char	*dest, const char	*src, size_t size)
 {
 	size_t	index;
 
 	index = 0;
-	while (src [index] != 0 && index < (size - 1))
+	if (src[index] == '\0')
+	{
+		dest[index] = '\0';
+		return (0);
+	}
+	if (size == 0)
+		return (ft_strlen (src));
+	while (src[index] != '\0' && index < (size -1))
 	{
 		dest[index] = src[index];
 		index++;
 	}
-	dest [index] = 0;
-	while (src [index] != 0)
-	{
+	dest[index] = '\0';
+	while (src[index] != '\0')
 		index++;
-	}
-	return (index);
+	return (ft_strlen(src));
 }
 /*
 int	main(void)
 {
-	char	buffer [8];
-	int		lenght;
+	char	buffer[0];
+ 	int		len;
 
-	lenght = ft_strlcpy(buffer, "Hola", 8);
-	printf("%s\n", buffer);
-	lenght = ft_strlcpy(buffer, "Torrelodones", 8);
-	printf("%s\n", buffer);
-	char dest[1000];
-	printf("%d\n", ft_strlcpy(dest, "lorem ipsum dolor sit amet", 0));
+	len = ft_strlcpy (buffer, "Hola", 3);
+	printf ("%d\n", len);
+	printf("%s", buffer);
 	return (0);
 }*/
