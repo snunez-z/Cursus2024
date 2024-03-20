caca40b672b62c61571f317ade197f2966e57fe1
Susana Nunez Zamora <snunez-z@c2r8s3.42madrid.com>
2024-03-20 11:48:01 +0100

    cambios en la funcion prioncipal

--- cursustelelana/get_next_line/get_next_line.c
+++ cursustelelana/get_next_line/get_next_line.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snunez-z <snunez-z@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 07:36:02 by snunez-z          #+#    #+#             */
+/*   Updated: 2024/03/20 11:45:42 by snunez-z         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -15,11 +26,25 @@ char	*terminate_and_reduce(char	*buffer, size_t used_chars,
 			size_t buffer_size)
 {
 	buffer[used_chars + 1] = '\0';
-	if(used_chars < buffer_size)
+	if (used_chars < buffer_size)
 		buffer = ft_reduce(buffer, used_chars + 2);
 	return (buffer);
 }
 
+static	char	*handle_buffer_growth(char	*buffer, size_t *current_size, size_t used_chars, int bytes_read, char ch)
+{	
+	if (bytes_read > 0)
+	{
+		if (++used_chars >= current_size)
+		{
+			buffer = ft_realloc(buffer, current_size);
+			if (buffer == NULL)
+				return (NULL);
+		}
+		buffer[used_chars] = ch;
+	}
+	return (buffer);
+}
 char	*get_next_line(int fd)
 {
 	char	*buffer;
@@ -28,27 +53,33 @@ char	*get_next_line(int fd)
 	int		bytes_read;
 	char	ch;
 
-	if((buffer = malloc ((MALLOC_SIZE + 1) * sizeof (char))) == NULL)
+	buffer = malloc ((MALLOC_SIZE + 1) * sizeof (char));
+	if (buffer == NULL)
 		return (NULL);
-	if ((bytes_read = read_one_char(fd, buffer)) <= 0)
-		return free_buffer(buffer);
+	bytes_read = read_one_char(fd, buffer);
+	if (bytes_read <= 0)
+		return (free_buffer(buffer));
 	current_size = MALLOC_SIZE;
 	used_chars = 0;
 	while (bytes_read > 0 && buffer[used_chars] != '\n')
 	{
-		if ((bytes_read = read_one_char (fd, &ch)) == -1)
-			return free_buffer(buffer);
+		bytes_read = read_one_char (fd, &ch);
+		if( bytes_read == -1)
+			return (free_buffer(buffer));
 		if (bytes_read > 0)
 		{
-			if ((++used_chars) >= current_size)
-				if ((buffer = ft_realloc(buffer, &current_size))  == NULL)
+			/*if (++used_chars >= current_size)
+			{
+				buffer = ft_realloc(buffer, &current_size); 
+				if (buffer == NULL)
 					return (NULL);
-			buffer[used_chars] = ch;
+			}
+			buffer[used_chars] = ch;*/
 		}
+		buffer = handle_buffer_growth(buffer, &current_size, used_chars, bytes_read, ch);
 	}
 	return (terminate_and_reduce(buffer, used_chars, current_size));
 }
-/*
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -62,7 +93,6 @@ int	main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-
 	fd = open(argv[1], O_RDONLY);
 	line = get_next_line (fd);
 	while(line != NULL)
@@ -73,4 +103,4 @@ int	main(int argc, char **argv)
 	}
 	close (fd);
 	return (0);
-} */
+} 
