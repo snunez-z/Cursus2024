2911038a57b8074688c425fa67498446b83f8882
Miguel Angel García Grande <mikioma@gmail.com>
2024-04-16 09:12:22 +0200

    Recursividad

---
+++ cursustelelana/ejercicios/recursividad1.c
@@ -0,0 +1,14 @@
+void putstr(const char *str)
+{
+    // Haz esta función. Lo que tiene que hacer es escribir (write) por pantalla (1)
+    // la cadena de caracteres "str", pero tiene que hacerlo carácter a carácter
+    // Hazlo con un bucle, te será más fácil, pero luego cambialo a hacerlo de forma
+    // recursiva
+}
+
+int main(void)
+{
+    putstr("Hola Sushi!!\n");
+    return (0);
+}
+
---
+++ cursustelelana/ejercicios/recursividad2.c
@@ -0,0 +1,15 @@
+void putstr_reverse(const char *str)
+{
+    // Haz esta función. Lo que tiene que hacer es escribir (write) por pantalla (1)
+    // la cadena de caracteres "str" pero al revés; es decir, si te pasan "Hola",
+    // tiene que escribir "aloH"
+    // Hazlo con recursividad
+    // PISTA: La solución es como el anterior... pero cambiando el orden de una cosa
+}
+
+int main(void)
+{
+    putstr_reverse("!!ihsuS aloH\n");
+    return (0);
+}
+
