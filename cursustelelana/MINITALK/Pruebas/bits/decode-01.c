84e7b335cbe40d30029886d185bb97de6e7457af
Miguel Angel García Grande <mikioma@gmail.com>
2024-06-11 10:44:07 +0200

    Decodificar #1

---
+++ cursustelelana/MINITALK/Pruebas/bits/decode-01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int bits[8] = { 0, 1, 0, 0, 0, 0, 0, 1 };
+    int resultado;
+
+    // Tienes que convertir ese array de bits en un número decimal
+    // 01000001 es el 65
+    // Así es que tienes que plantear como convertir esos bits en
+    // un int que valga 65 y guardar ese valor en "resultado", de forma que,
+    // si lo has hecho bien, en el printf del final tiene que salir 65
+
+    // .... haz tus cosas aquí
+
+
+    printf("El número es %d\n", resultado);
+    return (0);
+}
+
