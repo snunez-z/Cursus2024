efb349c5ee40b19a85b9430aceea0dc07aaf8a99
Susana Nuñez Zamora <susa.nunez@hotmail.com>
2024-07-12 20:25:04 +0200

    Rank02 level_3

---
+++ cursustelelana/Rank01/level_2/wdmatch.c
@@ -0,0 +1,68 @@
+/*Assignment name  : wdmatch
+Expected files   : wdmatch.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and checks whether it's possible to
+write the first string with characters from the second string, while respecting
+the order in which these characters appear in the second string.
+
+If it's possible, the program displays the string, followed by a \n, otherwise
+it simply displays a \n.
+
+If the number of arguments is not 2, the program displays a \n.
+
+Examples:
+
+$>./wdmatch "faya" "fgvvfdxcacpolhyghbreda" | cat -e
+faya$
+$>./wdmatch "faya" "fgvvfdxcacpolhyghbred" | cat -e
+$
+$>./wdmatch "forty two" "qfqfsoudf arzgrsayns tsryegftdgs sjytwdekuooixq " | cat -e
+forty two$
+$>./wdmatch "error" rrerrrfiiljdfxjyuifrrvcoojh | cat -e
+$
+$>./wdmatch | cat -e
+$*/
+
+#include <unistd.h>
+
+size_t	ft_strlen(char	*str)
+{
+	size_t	i;
+
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	is_possible(char *str1, char *str2)
+{
+	int	i;
+	int j;
+	i = 0;
+	j = 0;
+	while(str1[i] != '\0')
+	{
+		while (str2[j] != '\0' && str1[i] != str2[j])
+			j++;
+		if (str2[j] == '\0')
+			return (0);
+		if (str1[i] == str2[j])
+			j++;
+		i++;
+	}
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		if (is_possible (argv[1], argv[2]) == 1)
+			write (1, argv[1],ft_strlen(argv[1]));
+	}
+	write (1, "\n", 1);
+	return (0);	
+}
---
+++ cursustelelana/Rank01/level_3/add_prime_sum.c
@@ -0,0 +1,85 @@
+/*Assignment name  : add_prime_sum
+Expected files   : add_prime_sum.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+
+Yes, the examples are right.
+
+Examples:
+
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$>*/
+
+#include <unistd.h>
+
+int	is_prime (int num)
+{
+	int i;
+
+	i = 2;
+	while (i < num)
+	{
+		if ((num % i) == 0)
+		   return (0);
+		i++;	
+	}
+	return (1);
+}
+
+int	ft_atoi (char	*num)
+{
+	int value;
+	
+	value = 0;
+	while (*num != '\0')
+	{
+		value = (value * 10) + (*num - '0');
+		num ++;
+	}
+	return (value);	
+}
+
+void	put_nbr(int num)
+{
+	 char	digit;
+
+	if (num >= 10)
+		put_nbr (num / 10);
+	digit = (num % 10) + '0';
+	write (1, &digit, 1);
+}
+
+int	main (int argc, char	**argv)
+{
+	int num;
+	int	add;
+
+	if (argc < 2 || argv[1][0] == '-')
+	{
+		write (1, "0\n", 2);
+		return (0);
+	}
+	
+	num = ft_atoi (argv[1]);
+		add = 0;
+		while (num > 1)
+		{
+			if(is_prime (num) == 1) 
+				add = add + num; 
+			num --;
+		}
+	put_nbr(add);
+	write (1, "\n", 1);
+	return (0);
+}
