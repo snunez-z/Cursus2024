e234d7d91eee8be1ab8248d384b2250436ced6c7
Susana Nuñez Zamora <susa.nunez@hotmail.com>
2024-10-01 18:05:36 +0200

    changes

--- so_long/build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# gcc -I ../../MLX42/include/ -Wall -Werror -Wextra *.c -L ../../MLX42/build/ -lmlx42 -lX11 -lglfw
-gcc -I ./minilibx -Wall -Werror -Wextra *.c -L ./minilibx -lmlx -lX11 -lXext
