c1b336be20ea6e06534187904ddd6d22f6272946
Susana Nuñez Zamora <susa.nunez@hotmail.com>
2024-10-01 08:39:51 +0200

    Changes

--- Exam_Rank_02/level_4/ft_list_foreach/ft_list_foreach.c
+++ Exam_Rank_02/level_4/ft_list_foreach/ft_list_foreach.c
@@ -14,11 +14,11 @@
 #include "ft_list.h"
 #include <stdlib.h>
 
-void	ft_list_foreach (t_list *begin, void (*f) (void *))
+void	ft_list_foreach (t_list *begin_list, void (*f) (void *))
 {
 	t_list	*list_ptr;
 
-	list_ptr = begin;
+	list_ptr = begin_list;
 	while(list_ptr != NULL)
 	{
 		(*f) (list_ptr->data);
