70228e51879243d65b6f07d006db1b5e4fc2a551
Susana Nuñez Zamora <susa.nunez@hotmail.com>
2025-01-03 14:24:01 +0100

    push_swap

---
+++ push_swap/bonus/bonus.c
@@ -0,0 +1,96 @@
+ #include <unistd.h>
+ #include <stddef.h>
+ #include "ft_printf.h"
+ #include "push_swap.h"
+ #include "libft.h"
+
+static int read_line(int fd, char *buffer, size_t bytes_max)
+{
+    char    ch;
+    int read_result;
+    size_t index;
+
+    index = 0;
+    read_result = read(fd,&ch, 1);
+    while(read_result == 1 && index < bytes_max && ch != '\n')
+    {
+        buffer[index] = ch;
+        index++;
+        read_result = read (fd, &ch, 1);
+    }
+    buffer [index] = '\0';
+    if (read_result < 0 || index >= bytes_max)
+        return (-1);
+    return (index);
+} 
+    
+int	main (int argc, char	**argv)
+{
+	int				i;
+    char    buffer[10];
+    int read_result;
+	t_stack_list	*a;
+	t_stack_list	*b;
+
+    if(argc > 1)
+	{
+		i = 1;
+		a = NULL;
+		b = NULL;
+		while (i < argc)
+		{
+			if (!parse_argv(argv[i], &a))
+			{
+				list_destroy (a);
+				list_destroy (b);
+				ft_printf("Error\n");
+				return (-1);
+			}
+			i++;
+		}
+        read_result = read_line (0, buffer, 9);
+        while (read_result > 0)
+        {
+            if (ft_strncmp(buffer, "ra", 3) == 0)
+                ra(&a, 0);
+            else if( ft_strncmp (buffer, "rb", 3) == 0)
+                rb(&b, 0);
+            else if( ft_strncmp (buffer, "rr", 3) == 0)
+                rr(&a, &b, 0);
+            else if( ft_strncmp (buffer, "rra", 4) == 0)
+                rra(&a, 0);
+            else if( ft_strncmp (buffer, "rrb", 4) == 0)
+                rrb(&b, 0);
+            else if( ft_strncmp (buffer, "rrr", 4) == 0)
+                rrr(&a, &b, 0);
+            else if( ft_strncmp (buffer, "sa", 3) == 0)
+                sa(a, 0);    
+            else if( ft_strncmp (buffer, "sb", 3) == 0)
+                sb(b, 0);
+            else if( ft_strncmp (buffer, "ss", 3) == 0)
+                ss(a, b, 0); 
+            else if( ft_strncmp (buffer, "pa", 3) == 0)
+                pa(&a, &b, 0);
+            else if ( ft_strncmp (buffer, "pb", 3) == 0)
+                pb(&a, &b, 0);  
+           read_result = read_line (0, buffer, 9);
+        }
+
+        if (read_result < 0)
+        {
+            ft_printf("Error\n");
+            return (0);
+        }
+        if (is_sorted(a, 0) && list_size (b) == 0)
+            ft_printf("OK\n");
+        else
+            ft_printf("KO\n");
+      	list_destroy(a);
+		list_destroy(b);
+		return (0);
+	}
+}
+
+
+
+
